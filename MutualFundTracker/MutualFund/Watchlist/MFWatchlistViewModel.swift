bb0b7d2fe6eeb7ac57977c08371e0184accc9d1c
Rajesh RAMSHETTY SIDDARAJU <rajesh.ramshettysiddaraju@amadeus.com>
2021-09-01 00:35:48 +0530

    Used Boxing for data-binding instead of delegates for binding viewmodel data in view

--- MutualFundTracker/MutualFund/Watchlist/MFWatchlistViewModel.swift
+++ MutualFundTracker/MutualFund/Watchlist/MFWatchlistViewModel.swift
@@ -8,11 +8,6 @@
 import Foundation
 import CoreData
 
-
-protocol MFWatchlistViewModelDelegate: class {
-    func reloadData()
-}
-
 struct MFWatchlistSectionModels {
     var watchlistCellModels: [MFWatchlistTableViewCellModel]?
     init(watchlistCellModels: [MFWatchlistTableViewCellModel]?) {
@@ -22,21 +17,18 @@ struct MFWatchlistSectionModels {
 
 class MFWatchlistViewModel: NSObject {
     
-    weak var delegate: MFWatchlistViewModelDelegate?
-    var sections = [MFWatchlistSectionModels]()
+    var sections = Box([MFWatchlistSectionModels]())
     var mfManager = MutualFundManager()
     
     func loadWishlistedMutualFunds() {
-        print("loadMutualFunds")
-        sections = [MFWatchlistSectionModels]()
+        sections.value = [MFWatchlistSectionModels]()
         if let mutualFunds = mfManager.fetchWishListedMutualFunds() {
             var mutualFundModels = [MFWatchlistTableViewCellModel]()
             for mutualFund in mutualFunds {
                 mutualFundModels.append(MFWatchlistTableViewCellModel.init(mfScheme: mutualFund))
             }
-            self.sections.append(MFWatchlistSectionModels.init(watchlistCellModels: mutualFundModels))
+            self.sections.value.append(MFWatchlistSectionModels.init(watchlistCellModels: mutualFundModels))
         }
-        self.delegate?.reloadData()
     }
     
     func didSelectMutualFund(code: String) {
@@ -47,12 +39,12 @@ class MFWatchlistViewModel: NSObject {
     }
     
     func handleDeleteMF(at indexPath: IndexPath) {
-        guard let models = sections[indexPath.section].watchlistCellModels,
+        guard let models = sections.value[indexPath.section].watchlistCellModels,
               models.count > indexPath.row,
               let mfCode = models[indexPath.row].code else {
             return
         }
-        sections[indexPath.section].watchlistCellModels?.remove(at: indexPath.row)
+        sections.value[indexPath.section].watchlistCellModels?.remove(at: indexPath.row)
         mfManager.deleteSavedMutualFunds(codes: [mfCode])
     }
     
---
+++ MutualFundTracker/Utils/Box.swift
@@ -0,0 +1,30 @@
+//
+//  Box.swift
+//  MutualFundTracker
+//
+//  Created by Rajesh RAMSHETTY SIDDARAJU on 31/08/21.
+//
+
+import Foundation
+
+final class Box<T> {
+    
+    typealias Listener = (T) -> Void
+    var listener: Listener?
+    
+    var value: T {
+        didSet {
+            listener?(value)
+        }
+    }
+    
+    init(_ value: T) {
+        self.value = value
+    }
+    
+    func bind(listener: Listener?) {
+        self.listener = listener
+        listener?(value)
+    }
+    
+}
